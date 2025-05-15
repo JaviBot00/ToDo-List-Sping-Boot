4f9c75d9cb459f6d6f6e210c1ac8555852779c25
HotGuy <javibot00@gmail.com>
2025-05-15 21:25:51 +0200

    [master] Refactor AuthRequest to a record and add @NonNull annotations Converted `AuthRequest` class to a Java record for simpler and more concise representation. Added `@NonNull` annotations to `doFilterInternal` method parameters in `JwtFilter` to enforce null safety.

--- src/main/java/com/hotguy/tareas/dto/AuthRequest.java
+++ src/main/java/com/hotguy/tareas/dto/AuthRequest.java
@@ -1,13 +1,4 @@
 package com.hotguy.tareas.dto;
 
-public class AuthRequest {
-    private final String token;
-
-    public AuthRequest(String token) {
-        this.token = token;
-    }
-
-    public String getToken() {
-        return token;
-    }
+public record AuthRequest(String token) {
 }
